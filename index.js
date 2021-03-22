54c19af687c290199677c0511b2fc9af02383a0e
Andy0910 <andres.gonzalez42@hotmail.com>
2021-03-21 21:06:55 -0600

    commit inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var folder = path.join(__dirname, '/public');
+
+app.use(express.static(folder));
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
